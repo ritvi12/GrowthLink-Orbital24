9b3071e593ca32260635b2ad3a7d3c070ff704ff
ritvi12 <ritvi0205@gmail.com>
2024-05-28 09:33:10 +0530

    SideBar Updated

--- growthlink/src/components/NavBar.js
+++ growthlink/src/components/NavBar.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { NavBarData } from './NavBarData'
 import './NavBar.css'
 import { IconContext } from 'react-icons'
+import { Button } from './Button'
 
 const NavBar = () => {
     const [sidebar, setSideBar] = useState(false)
+    const [button, setButton] = useState(true);
 
     const showSideBar = () => setSideBar(!sidebar)
+
+    const showButton = () => {
+      if (window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+  
+    useEffect(() => {
+      showButton();
+    }, []);
+  
+    window.addEventListener('resize', showButton);
   return (
     <>
     <IconContext.Provider value={{color: 'black'}}>
       <div className='navbar'>
-        <Link to="#" className='menu-bars'>
+        <Link to="/" className='menu-bars'>
             <FaIcons.FaBars onClick={showSideBar} />
         </Link>
+        <div className='Name'>GROWTHLINK</div>
       </div>
       <nav className= {sidebar ? 'nav-menu active' : 'nav-menu'}>
         <ul className='nav-menu-items' onClick={showSideBar}>
@@ -35,7 +52,11 @@ const NavBar = () => {
                     </li>
                 )
             })}
+            <div className='button'>
+              {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+            </div>
         </ul>
+        
       </nav>
       </IconContext.Provider>
     </>
