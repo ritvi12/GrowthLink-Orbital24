dabb6f8dfb79948453fe00718f4242a84c2edb13
annabellekk <yuyien.khoo@gmail.com>
2024-07-20 17:41:40 +0800

    Footer

--- growthlink/src/components/Footer.js
+++ growthlink/src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./Footer.css";
+import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer>
       <div class="footer-col">
-        <h4>Contact us</h4>
+        <h4>
+        <FaRegCopyright />  NUS Orbital Team Anari 2024
+        </h4>
         <ul>
           <li><a href="#">one</a></li>
           <li><a href="#">two</a></li>
