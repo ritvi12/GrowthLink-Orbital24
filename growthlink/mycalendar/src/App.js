8d0627eded88bbd0921cccdb6f3045aca533364d
annabellekk <yuyien.khoo@gmail.com>
2024-07-26 09:40:53 +0800

    Calendar page

---
+++ growthlink/mycalendar/src/App.js
@@ -0,0 +1,11 @@
+import Calendar from "./Calendar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
---
+++ growthlink/mycalendar/src/Calendar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+function Calendar() {
+    return <div>
+        <Fullcalendar 
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView={"dayGridMonth"}
+        />
+    </div>;
+}
+$
+export default Calendar;
